242fa0854e9621fa13c3adeefaaa46000a963a8a
danielkingori <kingndiritu@gmail.com>
2024-09-11 08:57:26 +0300

    advanced security features

--- ecomerce/users/models.py
+++ ecomerce/users/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password):
+        if not email:
+            raise ValueError('Email is required')
+        user = self.model(email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+        
+    def create_superuser(self, email, password):
+        user = self.create_user(email=email, password=password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
+        
+    
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=255)
+    username = models.CharField(unique=False, max_length=15)
+    objects = UserManager()
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
