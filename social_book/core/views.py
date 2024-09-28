d64fc588318cbe743e512556a319101add3b8e42
danielkingori <kingndiritu@gmail.com>
2024-09-28 22:15:41 +0300

    advanced security features

--- social_book/core/views.py
+++ social_book/core/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
-from .models import Profile, Post, LikePost
+from .models import Profile, Post, LikePost, FollowersCount
 from django.contrib.auth.decorators import login_required
 # Create your views here.
 
@@ -29,7 +29,7 @@ def upload(request):
         return redirect('/')
     else:
         return redirect('/')
-    return HttpResponse('<h1>upload view</h1>')
+    
 
 
 @login_required(login_url='signin')
@@ -51,6 +51,24 @@ def like_post(request):
         post.save()
         return redirect('/') 
 
+@login_required(login_url='signin')
+def follow(request):
+    if request.method == 'POST':
+        follower = request.POST['follower']
+        user = request.POST['user']
+        
+        if FollowersCount.objects.filter(follower=follower, user=user).first():
+            delete_follower = FollowersCount.objects.get(follower=follower, user=user)
+            delete_follower.delete()
+            return redirect('/profile/'+user)
+        else:
+            new_follower =  FollowersCount.objects.create(follower=follower, user=user)
+            new_follower.save()
+            return redirect('/profile/'+user)
+        
+    else:
+        return redirect('/')
+    
 
 @login_required(login_url='signin')
 def profile(request, pk):
@@ -58,12 +76,26 @@ def profile(request, pk):
     user_profile = Profile.objects.get(user=user_object)
     user_posts = Post.objects.filter(user=pk)
     user_post_length = len(user_posts)
+    follower = request.user.username
+    user = pk
+    
+    if FollowersCount.objects.filter(follower=follower, user=user).first():
+        button_text = 'Unfollow'
+    else:
+        button_text = 'Follow'
+        
+    user_followers = len(FollowersCount.objects.filter(user=pk))
+    user_following = len(FollowersCount.objects.filter(follower=pk))
     
     context = {
         'user_object':user_object,
         'user_profile':user_profile,
         'user_posts':user_posts,
         'user_post_length':user_post_length,
+        'button_text':button_text,
+        'user_followers':user_followers,
+        'user_following':user_following,
+        
         
     }
     return render(request, 'profile.html', context)
@@ -125,7 +157,7 @@ def signup(request):
                 return redirect('settings')
                 
         else:
-            messages.info(request, 'Password Not Marching')
+            messages.info(request, 'Password Not Matching')
             return redirect('signup')
             
     else:
