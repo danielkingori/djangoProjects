a7d47a128a790f591364090f5b6acb10ff13b8a1
danielkingori <kingndiritu@gmail.com>
2024-09-28 12:17:17 +0300

    calories counter

--- CalorieCounter/counter/views.py
+++ CalorieCounter/counter/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+import requests
+
 
 # Create your views here.
 def home(request):
+    query = '1lb brisket and fries'
+    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
+    response = requests.get(api_url, headers={'X-Api-Key': 'sx7sc1mQ0y5we6XE8+KpfQ==E2IglMwdKTgmaSKA'})
+    if response.status_code == requests.codes.ok:
+        print(response.text)
+    else:
+        print("Error:", response.status_code, response.text)
     return render(request, 'home.html')
