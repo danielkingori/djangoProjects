d9c913b888e0cdbf292d18d2339f149601b634a8
danielkingori <kingndiritu@gmail.com>
2024-09-28 13:08:25 +0300

    calories counter

--- CalorieCounter/counter/views.py
+++ CalorieCounter/counter/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 import requests
+import json
 
 
 # Create your views here.
 def home(request):
-    query = '1lb brisket and fries'
-    api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
-    response = requests.get(api_url, headers={'X-Api-Key': 'sx7sc1mQ0y5we6XE8+KpfQ==E2IglMwdKTgmaSKA'})
-    if response.status_code == requests.codes.ok:
-        print(response.text)
+    # query = '1lb brisket and fries'
+    # api_url = 'https://api.api-ninjas.com/v1/nutrition?query={}'.format(query)
+    # response = requests.get(api_url, headers={'X-Api-Key': 'sx7sc1mQ0y5we6XE8+KpfQ==E2IglMwdKTgmaSKA'})
+    # if response.status_code == requests.codes.ok:
+    #     print(response.text)
+    # else:
+    #     print("Error:", response.status_code, response.text)
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(api_url + query, headers = {'X-Api-Key': 'sx7sc1mQ0y5we6XE8+KpfQ==E2IglMwdKTgmaSKA'})
+        try:
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = "oops there was an error"
+            print(e)
+        return render(request, 'home.html', {'api':api})
     else:
-        print("Error:", response.status_code, response.text)
-    return render(request, 'home.html')
+        return render(request, 'home.html', {'query':'enter a valid query'})
+
+ 
+
+#1lb brisket and fries
